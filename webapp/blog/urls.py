a148748bde74ca474064c249d9fa8d7922d3411a
jacobtj <66802943+jacobtj@users.noreply.github.com>
2023-04-22 17:31:14 -0700

    added creating/deleting post functionality

--- webapp/blog/urls.py
+++ webapp/blog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from .views import PostListView, PostCreateView, PostDeleteView, PostDetailView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
 ]
 
